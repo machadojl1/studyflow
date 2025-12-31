b14b8752c3a7fa32009d4966f0328e9ebdc77563
machadojl1 <joaolucasmatos07@gmail.com>
2025-12-31 11:17:57 -0300

    B: fetch functionalities for create and save. Need work on app.py and templates

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/create")
+def create():
+    return render_template("create.html")
+
+
+@app.route("/save")
+def save():
+    return render_template("save.html")
